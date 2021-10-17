8e324b66204dd937bb969832d513f99d05a5f2f6
石佳琪 <rt006814@rotai.com>
2021-10-17 15:15:03 +0800

    初始化项目

---
+++ src/test/java/FoodTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author ：sjq
+ * @date ：Created in 2021/10/17 下午3:08
+ * @description：
+ * @modified By：
+ * @version: $
+ */
+@SpringBootTest
+public class FoodTest {
+}
